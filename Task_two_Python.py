68fda3d444963081572a31a0c46a2ea90e1d3147
Sergei Milto <Milto_Sergei@student.ceu.edu>
2024-03-08 17:27:09 +0100

    Deleted the files from the previous folder

--- Task_two_Python.py
+++
@@ -1,8 +0,0 @@
-#Demonstrating a folder structure and dictionaries.
-dictionary_A = {"material": "glass", "color": "blue", "shape": "round", "age": 30}
-for key, value in dictionary_A.items():
-  print("The dictionary:", key, ":", value)
-
-for key, value in dictionary_A.items():
-  if type(value) == int:
-    print("Info in numbers:", key, ":", value)
